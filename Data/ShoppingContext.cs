046856c9f816f24d5051bc4c0946b4316947af83
Ianina Voda <yaninavoda@gmail.com>
2024-02-18 23:54:49 +0200

    add dbcontext and connection string

---
+++ Data/ShoppingContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingAppDev.Models;
+
+namespace ShoppingAppDev.Data
+{
+    public class ShoppingContext : DbContext
+    {
+        public ShoppingContext(DbContextOptions<ShoppingContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Supermarket> Supermarkets { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}
+
